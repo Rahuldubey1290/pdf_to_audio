e454d87168bd2678e3b8693c7c94823648b4963e
RahulDubey <rahuldubey1290@gmail.com>
2022-05-29 19:57:39 +0530

    Initial Commit

--- main6.py
+++ main6.py
@@ -3,16 +3,25 @@ import PyPDF2
 
 def main():
     try:
-        with open('Stephen Hawking - A Brief History Of Time.pdf', 'rb') as pdfFileObj:
+        with open('Presentation1.pdf', 'rb') as pdfFileObj:
             pdfReader = PyPDF2.PdfFileReader(pdfFileObj, strict=False)
             pages = pdfReader.numPages
             print(f"The total number of pages is :{pages}")
             inp = int(input('Press 1 for single page and 2 for pages and 3 for all pages\n'))
+
+            engine = pyttsx3.init()
+            rate = engine.getProperty('rate')
+            engine.setProperty('rate', 170)
+
             if inp==1:
                 readPage = int(input('Enter the page you want to read:\n'))
                 pageObj = pdfReader.getPage(readPage)
                 text = (pageObj.extractText())
                 print(text)
+                wantListen = (input('Do you want to listen this ?. Yes Or No \n'))
+                if 'yes' in wantListen:
+                    engine.say(text)
+                    engine.runAndWait()
 
             elif inp==2:
                 givenPage1 = int(input('Enter the starting page number:\n'))
@@ -21,23 +30,17 @@ def main():
                     pageObj = pdfReader.getPage(page)
                     text = (pageObj.extractText())
                     print(text)
+                    engine.say(text)
+                    engine.runAndWait()
 
             else:
                  for page in range(pages):
                      pageObj = pdfReader.getPage(page)
                      text= (pageObj.extractText())
                      print(text)
+                     engine.say(text)
+                     engine.runAndWait()
 
-
-            wantListen = (input('Do you want to listen this ?. Yes Or No \n'))
-            if 'yes' in wantListen:
-                engine = pyttsx3.init()
-                rate = engine.getProperty('rate')
-                engine.setProperty('rate', 126)
-                engine.say(text)
-                engine.runAndWait()
-            else:
-                exit()
     except Exception as e:
         print(e)
 
